0b80d1959648e09a5cc89e40a15b22ccd4f22132
nhungnhung <nhungdt21411c@st.uel.edu.vn>
2024-04-21 16:06:50 +0700

    .

---
+++ app/src/main/java/com/thaikimhuynh/miladyapp/AdminProductManagementActivity.java
@@ -0,0 +1,14 @@
+package com.thaikimhuynh.miladyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminProductManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_product_management);
+    }
+}

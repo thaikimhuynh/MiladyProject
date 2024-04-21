b717801f27cbac4f851043626d7d8a6fcca86791
kimhuynh <thaikimhuynh66@gmail.com>
2024-04-21 12:05:02 +0700

    .

--- app/src/main/java/com/thaikimhuynh/miladyapp/WelcomeActivity.java
+++ app/src/main/java/com/thaikimhuynh/miladyapp/WelcomeActivity.java
@@ -2,13 +2,33 @@ package com.thaikimhuynh.miladyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+Button btnHadAccount, btnStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        addViews();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnHadAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void addViews() {
+        btnHadAccount=findViewById(R.id.btnHadAccount);
+        btnStarted=findViewById(R.id.btnStarted);
     }
 }

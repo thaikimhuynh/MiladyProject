93c883271ab0eb3b57762d93f1f3d54f771512be
nhungnhung <nhungdt21411c@st.uel.edu.vn>
2024-04-22 22:42:59 +0700

    .

---
+++ app/src/main/java/com/thaikimhuynh/miladyapp/admin/AdminOrderManagementActivity.java
@@ -0,0 +1,16 @@
+package com.thaikimhuynh.miladyapp.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thaikimhuynh.miladyapp.R;
+
+public class AdminOrderManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_order_management);
+    }
+}

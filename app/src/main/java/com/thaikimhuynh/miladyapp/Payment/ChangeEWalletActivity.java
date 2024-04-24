60ff5495b65ae752ee2b8d83c97eb6fdece80faf
HuyPhan21411C <huypg21411c@st.uel.edu.vn>
2024-04-24 17:42:43 +0700

    Payment

---
+++ app/src/main/java/com/thaikimhuynh/miladyapp/Payment/ChangeEWalletActivity.java
@@ -0,0 +1,16 @@
+package com.thaikimhuynh.miladyapp.Payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thaikimhuynh.miladyapp.R;
+
+public class ChangeEWalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_ewallet);
+    }
+}

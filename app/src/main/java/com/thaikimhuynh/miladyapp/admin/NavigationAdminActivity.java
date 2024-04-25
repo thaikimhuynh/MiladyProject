f0f11c1a38aaeb905db957a896ffbc891611c477
nhungnhung <nhungdt21411c@st.uel.edu.vn>
2024-04-25 23:55:47 +0700

    .

---
+++ app/src/main/java/com/thaikimhuynh/miladyapp/admin/NavigationAdminActivity.java
@@ -0,0 +1,18 @@
+package com.thaikimhuynh.miladyapp.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.thaikimhuynh.miladyapp.R;
+
+public class NavigationAdminActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_admin);
+    }
+}

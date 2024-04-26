46bc8f086e5da9cc1cda2da53c4e5515d337b596
kimhuynh <thaikimhuynh66@gmail.com>
2024-04-26 11:54:04 +0700

    done fragment profile,setting activity, add xml banking wallet

--- app/src/main/java/com/thaikimhuynh/miladyapp/fragment/ProfileFragment.java
+++ app/src/main/java/com/thaikimhuynh/miladyapp/fragment/ProfileFragment.java
@@ -1,14 +1,24 @@
 package com.thaikimhuynh.miladyapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.thaikimhuynh.miladyapp.EarnPointActivity;
+import com.thaikimhuynh.miladyapp.HelpCenterActivity;
+import com.thaikimhuynh.miladyapp.MyOrdersActivity;
 import com.thaikimhuynh.miladyapp.R;
+import com.thaikimhuynh.miladyapp.SettingActivity;
+import com.thaikimhuynh.miladyapp.WishListActivity;
+import com.thaikimhuynh.miladyapp.payment.ViewPaymentMethodActivity;
+import com.thaikimhuynh.miladyapp.payment.WalletInformationActivity;
+import com.thaikimhuynh.miladyapp.profile.MyProfileActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +35,7 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    CardView cardMyProfile, cardMyOrders, cardMyWallet, cardEarnPoints, cardWishList, cardHelpCenter, cardSetting;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -57,10 +68,65 @@ public class ProfileFragment extends Fragment {
         }
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        cardMyProfile = view.findViewById(R.id.cardMyProfile);
+        cardMyOrders = view.findViewById(R.id.cardMyOrders);
+        cardMyWallet = view.findViewById(R.id.cardMyWallet);
+        cardEarnPoints = view.findViewById(R.id.cardEarnPoints);
+        cardWishList = view.findViewById(R.id.cardWishList);
+        cardHelpCenter = view.findViewById(R.id.cardHelpCenter);
+        cardSetting = view.findViewById(R.id.cardSetting);
+
+        cardMyProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MyProfileActivity.class);
+                startActivity(intent);            }
+        });
+        cardMyOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MyOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+        cardEarnPoints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), EarnPointActivity.class);
+                startActivity(intent);            }
+        });
+        cardMyWallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ViewPaymentMethodActivity.class);
+                startActivity(intent);            }
+        });
+        cardWishList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), WishListActivity.class);
+                startActivity(intent);            }
+        });
+        cardHelpCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), HelpCenterActivity.class);
+                startActivity(intent);            }
+        });
+        cardSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SettingActivity.class);
+                startActivity(intent);            }
+        });
+        return view;
     }
+
+
 }

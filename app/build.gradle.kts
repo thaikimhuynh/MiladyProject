af4294594337336c9b7fd6d971cedffc72b01331
TRUC LOAN <loan22224@gmail.com>
2024-05-05 13:01:55 +0700

    forgot password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,11 @@ android {
 
 dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-storage")
+    implementation ("com.google.firebase:firebase-auth:22.0.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")

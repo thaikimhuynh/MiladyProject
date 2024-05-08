3b0e676c56af03dfb170b9a0a29d5bf32093833a
TRUC LOAN <loan22224@gmail.com>
2024-05-08 18:48:43 +0700

    sign up activity

--- app/src/main/java/com/thaikimhuynh/miladyapp/signup/SignUpActivity.java
+++ app/src/main/java/com/thaikimhuynh/miladyapp/signup/SignUpActivity.java
@@ -1,42 +1,179 @@
 package com.thaikimhuynh.miladyapp.signup;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.thaikimhuynh.miladyapp.R;
+import com.thaikimhuynh.miladyapp.forgotpassword.ForgotPasswordActivity;
+import com.thaikimhuynh.miladyapp.forgotpassword.VerifyCodePasswordActivity;
+import com.thaikimhuynh.miladyapp.login.LoginActivity;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignUpActivity extends AppCompatActivity {
+    private static final String TAG = VerifyAccountActivity.class.getName();
+    EditText edtPhoneNumber, edtEmail, edtPassword;
     TextView txtLogin;
     Button btnSignUp;
+    String phoneNumber;
+    String Email;
+    String Password;
+    private DatabaseReference mDatabase;
+    private static final String COUNTRY_CODE = "+84";
+
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    private TextView txtMessageError;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        addViews();
-        addEvents();
+        addView();
+        database= FirebaseDatabase.getInstance();
+        reference= database.getReference("User");
+//                HelperClass helperClass= new HelperClass(PhoneNumber, Email, Password);
+//                reference.child(PhoneNumber).setValue(helperClass);
+//                Toast.makeText(SignUpActivity.this,"Signup successfully",Toast.LENGTH_SHORT).show();
+
+//                edtPhoneNumber= findViewById(R.id.edtPhoneNumber);
+//                edtEmail=findViewById(R.id.edtEmail);
+//                edtPassword=findViewById(R.id.edtPassword);
+//                txtLogin= findViewById(R.id.txtLogin);
+//                btnSignUp=findViewById(R.id.btnSignUp);
+//                txtMessageError = findViewById(R.id.txtMessageError);
+
     }
 
-    private void addEvents() {
+    private void addView() {
+        edtPhoneNumber= findViewById(R.id.edtPhoneNumber);
+        btnSignUp=findViewById(R.id.btnSignUp);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        txtMessageError = findViewById(R.id.txtMessageError);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(SignUpActivity.this, VerifyAccountActivity.class);
-                startActivity(intent);
+                phoneNumber = edtPhoneNumber.getText().toString().trim();
+
+                if (TextUtils.isEmpty(phoneNumber)) {
+                    txtMessageError.setText("Please enter a phone number");
+                } else if (!isValidPhoneNumber(phoneNumber)) {
+                    txtMessageError.setText("Please enter a valid phone number");
+                } else
+                {
+                    if (phoneNumber.startsWith("+84"))
+                    {
+                        phoneNumber = "0" + phoneNumber.substring(3);
+                    }
+                }
+                Email = edtEmail.getText().toString();
+                Password = edtPassword.getText().toString();
+                checkPhoneNumberExist(phoneNumber);
+
             }
         });
     }
 
-    private void addViews() {
-        txtLogin=findViewById(R.id.txtLogin);
-        txtLogin.setPaintFlags(txtLogin.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    private void checkPhoneNumberExist(String phoneNumber) {
+        mDatabase = FirebaseDatabase.getInstance().getReference("User");
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            boolean isPhoneNumberExist = false;
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    String storedPhone = String.valueOf(userSnapshot.child("phoneNumber").getValue());
 
-        btnSignUp=findViewById(R.id.btnSignUp);
+                    // Kiểm tra xem số điện thoại từ DataSnapshot có trùng khớp với số điện thoại người dùng nhập không
+                    if (TextUtils.equals(storedPhone, phoneNumber)) {
+                        isPhoneNumberExist = true;
+                        txtMessageError.setText("This phone number was registered before");
+                        break;
+                    }
+                }
+
+                if (!isPhoneNumberExist)
+                {
+                    String phoneWithCountryCode = COUNTRY_CODE + phoneNumber.substring(1);
+                    Log.d("PhoneWithCountryCode", phoneWithCountryCode);
+
+                    sendOTP(phoneWithCountryCode);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(SignUpActivity.this, "Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e("Firebase Database", "Error: " + databaseError.getMessage());
+            }
+        });
     }
+
+    private void sendOTP(String phoneWithCountryCode) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneWithCountryCode,
+                60, // Timeout duration
+                TimeUnit.SECONDS,
+                SignUpActivity.this,
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        Log.d(TAG, "Sending intent to NewPasswordActivity with phoneNumber: " + phoneNumber);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Toast.makeText(SignUpActivity.this, "Verification failed", Toast.LENGTH_SHORT).show();                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verificationId,
+                                           @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                        Toast.makeText(SignUpActivity.this, "An OTP has been sent to your phone."
+                                ,Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(SignUpActivity.this, VerifyAccountActivity.class);
+                        intent.putExtra("phoneNumber", phoneNumber); // Truyền data qua Intent
+                        intent.putExtra("email",Email);
+                        intent.putExtra("password",Password);
+
+                        intent.putExtra("verificationId", verificationId);
+                        startActivity(intent);
+                    }
+                });
+    }
+
+    private boolean isValidPhoneNumber(final String phoneNumber) {
+        return phoneNumber.matches("^(\\+84|0)\\d{9}$");
+    }
+
 }
+
+
--- app/src/main/java/com/thaikimhuynh/miladyapp/signup/VerifyAccountActivity.java
+++ app/src/main/java/com/thaikimhuynh/miladyapp/signup/VerifyAccountActivity.java
@@ -1,26 +1,116 @@
 package com.thaikimhuynh.miladyapp.signup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Paint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.thaikimhuynh.miladyapp.R;
+import com.thaikimhuynh.miladyapp.login.LoginActivity;
+import com.thaikimhuynh.miladyapp.model.HelperClass;
 
 public class VerifyAccountActivity extends AppCompatActivity {
-    TextView txtLogin;
+    private EditText edtCode;
+    private Button btn;
+    private String verificationId;
+    private TextView txtMessageError;
+    private FirebaseAuth mAuth;
+    private String phoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_account);
-        addViews();
+        addView();
     }
 
-    private void addViews() {
-        txtLogin=findViewById(R.id.txtLogin);
-        txtLogin.setPaintFlags(txtLogin.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    private void addView() {
+        edtCode = findViewById(R.id.edtCode);
+        btn = findViewById(R.id.btn);
+        txtMessageError=findViewById(R.id.txtMessageError);
+        // Nhận verificationId từ Intent
+        verificationId = getIntent().getStringExtra("verificationId");
+        phoneNumber = getIntent().getStringExtra("phoneNumber");
 
+        // Khởi tạo FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        // Xử lý sự kiện khi nhấn nút Verify
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy mã xác minh từ người dùng
+                String code = edtCode.getText().toString().trim();
+
+                // Kiểm tra xem mã xác minh có rỗng hay không
+                if (!code.isEmpty()) {
+                    // Nếu không rỗng, gửi mã xác minh đến Firebase để xác minh
+                    verifyPhoneNumberWithCode(verificationId, code);
+                } else {
+                    // Nếu rỗng, hiển thị thông báo cho người dùng
+                    txtMessageError.setText("Please enter verification code");
+                }
+            }
+        });
     }
-};
+
+    private void verifyPhoneNumberWithCode(String verificationId, String code) {
+        // Tạo PhoneAuthCredential object từ mã xác minh và verificationId
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+
+        // Đăng nhập bằng PhoneAuthCredential
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            saveUserDataToFirebase();
+                            txtMessageError.setVisibility(View.GONE);
+                            // Nếu xác minh thành công, chuyển hướng người dùng đến NewPasswordActivity
+                            // Truyền số điện thoại qua Intent
+                            Intent intent = new Intent(VerifyAccountActivity.this, LoginActivity.class);
+                            intent.putExtra("phoneNumber", phoneNumber);
+                            startActivity(intent);
+                            finish(); // Kết thúc hoạt động hiện tại
+                        } else {
+                            // Xử lý khi xác minh không thành công
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                txtMessageError.setText("You have entered failed code. Please fill true code");
+                            } else {
+                                Toast.makeText(VerifyAccountActivity.this, "Verification failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void saveUserDataToFirebase() {
+        // Lấy thông tin người dùng từ Intent
+        String phoneNumber = getIntent().getStringExtra("phoneNumber");
+        String email = getIntent().getStringExtra("email");
+        String password = getIntent().getStringExtra("password");
+
+        // Tạo một đối tượng User từ thông tin nhập vào
+        HelperClass helperClass= new HelperClass(phoneNumber, email, password);
+
+        // Lưu thông tin người dùng vào cơ sở dữ liệu Firebase
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("User");
+        usersRef.child(phoneNumber).setValue(helperClass);
+    }
+}

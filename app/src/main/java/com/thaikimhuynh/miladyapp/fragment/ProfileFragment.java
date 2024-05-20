df4a9523c4cbed339d4b7f18f5dd0bea65997529
TRUC LOAN <loan22224@gmail.com>
2024-05-20 21:17:45 +0700

    multi language

--- app/src/main/java/com/thaikimhuynh/miladyapp/fragment/ProfileFragment.java
+++ app/src/main/java/com/thaikimhuynh/miladyapp/fragment/ProfileFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
